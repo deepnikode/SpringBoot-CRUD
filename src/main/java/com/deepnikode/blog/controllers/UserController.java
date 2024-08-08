682bbb8e08e8e286877d9a9f2ddc478d48a78ce1
deepnikode <deepnikode@gmail.com>
2024-08-08 10:59:53 +0530

    CRUD apis completed

--- src/main/java/com/deepnikode/blog/controllers/UserController.java
+++ src/main/java/com/deepnikode/blog/controllers/UserController.java
@@ -1,14 +1,21 @@
 package com.deepnikode.blog.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.deepnikode.blog.payloads.ApiResponse;
 import com.deepnikode.blog.payloads.UserDto;
 import com.deepnikode.blog.services.UserService;
 
@@ -24,13 +31,45 @@ public class UserController
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/")
+	
+	@PostMapping("/")
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
 	{
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
 	}
 	
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uId)
+	{	
+		UserDto updatedUser= this.userService.updateUser(userDto, uId);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer uId)
+	{
+		this.deleteUser(uId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully",true), HttpStatus.OK);
+	}	
+	
+	
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers()
+	{	
+		List<UserDto> userDto= this.userService.getAllUsers();
+		return ResponseEntity.ok(userDto);
+	}
+	
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUsers(@PathVariable Integer userId)
+	{	
+		UserDto userDto= this.userService.getUserById(userId);
+		return ResponseEntity.ok(userDto);
+	}
 	
 
 }
--- src/test/java/com/deepnikode/blog/BlogAppApisApplicationTests.java
+++ src/test/java/com/deepnikode/blog/BlogAppApisApplicationTests.java
@@ -1,13 +1,30 @@
 package com.deepnikode.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.deepnikode.blog.repositories.UserRepo;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
-
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	public void repoTest()
+	{
+		System.out.println(this.userRepo.getClass().getName());
+		System.out.println(this.userRepo.getClass().getPackageName());
+		
+	}
+	
+	
 
 }
